0da8b8dcfcae1a5a172826e595e00cbdc402238f
rumple <ruipeng.wu@cyclone-robotics.com>
2021-08-24 13:35:58 +0800

    up

--- control/middleware.go
+++ control/middleware.go
@@ -8,21 +8,32 @@ package control
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
+func DefaultCorsMap() map[string]string {
+	return map[string]string{
+		"Access-Control-Allow-Origin":      "*",
+		"Access-Control-Max-Age":           "86400",
+		"Access-Control-Allow-Methods":     "POST, GET, OPTIONS, PUT, DELETE, PATCH",
+		"Access-Control-Allow-Headers":     strings.Join(DefaultAllowHeaders(), ","),
+		"Access-Control-Expose-Headers":    "Content-Length",
+		"Access-Control-Allow-Credentials": "true",
+	}
+}
+
+func DefaultAllowHeaders() []string {
+	return []string{"Origin", "Content-Type", "Content-Length", " Accept-Encoding", "X-CSRF-VerifyToken", "Authorization"}
+}
+
 // Cors 处理跨域请求
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods",
-			"POST, GET, OPTIONS, PUT, DELETE, PATCH")
-		c.Writer.Header().Set("Access-Control-Allow-Headers",
-			"Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-VerifyToken, Authorization")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		for k, v := range DefaultCorsMap() {
+			c.Writer.Header().Set(k, v)
+		}
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusOK)
 		}
@@ -31,6 +42,21 @@ func Cors() gin.HandlerFunc {
 
 func NewCors(headers map[string]string) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		for k, v := range headers {
+			c.Writer.Header().Set(k, v)
+		}
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+		}
+	}
+}
+
+func NewCorsWithDefault(headers map[string]string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		for k, v := range DefaultCorsMap() {
+			c.Writer.Header().Set(k, v)
+		}
+
 		for k, v := range headers {
 			c.Writer.Header().Set(k, v)
 		}

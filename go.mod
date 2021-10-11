ec8c6011df973563b3be9c07468805eab6a7c773
rumple <ruipeng.wu@cyclone-robotics.com>
2021-10-11 16:17:17 +0800

    增加jwt

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/wuruipeng404/scaffold
 go 1.17
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.4
+	github.com/google/uuid v1.3.0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	go.uber.org/zap v1.19.0
 )
---
+++ util/jwt.go
@@ -0,0 +1,38 @@
+/*
+* @Author: Rumple
+* @Email: ruipeng.wu@cyclone-robotics.com
+* @DateTime: 2021/10/11 14:40
+ */
+
+package util
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
+)
+
+func JwtCreateToken(secret, sub string, expire time.Duration) (string, error) {
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(expire).Unix(), // 过期时间
+		Id:        uuid.New().String(),           // 唯一ID
+		IssuedAt:  time.Now().Unix(),             // 发行时间
+		Subject:   sub,                           // 用户信息
+	}).SignedString([]byte(secret))
+}
+
+func JwtParseToken(secret, token string) (sub string, err error) {
+	var (
+		claim *jwt.Token
+	)
+
+	if claim, err = jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	}); err != nil {
+		return
+	}
+
+	sub = claim.Claims.(jwt.MapClaims)["sub"].(string)
+	return
+}

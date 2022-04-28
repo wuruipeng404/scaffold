9fcd55e1f8d93788ff0a99f38097402e8913008b
Rumple <357711589@qq.com>
2022-04-28 14:47:03 +0800

    up

--- orm/sort/sort.go
+++ orm/sort/sort.go
@@ -1,9 +1,29 @@
 package sort
 
-// some default sort
-
-const (
-	CreateTimeDesc = "created_at desc"
-	UpdateTimeDesc = "updated_at desc"
-	IdDesc         = "id desc"
+import (
+	"fmt"
 )
+
+// some default sort
+const (
+	createTime = "created_at"
+	updateTime = "updated_at"
+	deleteTime = "deleted_at"
+	id         = "id"
+)
+
+var (
+	CreateTimeAsc  = Asc(createTime)
+	UpdateTimeAsc  = Asc(updateTime)
+	CreateTimeDesc = Desc(createTime)
+	UpdateTimeDesc = Desc(updateTime)
+	IdDesc         = Desc(id)
+)
+
+func Asc(field string) string {
+	return fmt.Sprintf("%s asc", field)
+}
+
+func Desc(field string) string {
+	return fmt.Sprintf("%s desc", field)
+}
